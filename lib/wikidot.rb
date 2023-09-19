5dbdc53ed96036012f386171238eb80515d71a07
Yuki Watanabe <ukwhatn@gmail.com>
2023-09-19 13:36:40 +0900

    init

---
+++ lib/wikidot.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wikidot/version"
+
+module Wikidot
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wikidot/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Wikidot
+  VERSION = "0.1.0"
+end
